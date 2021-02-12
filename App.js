24232629f90538e25a00e29e0f70ed6fd47e42ec
Lelanie Sedrick <lasedrish@gmail.com>
2021-02-12 07:23:52 -0600

    add Cat

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import  Cat from './Cat';
 
 export default function App() {
   return (
